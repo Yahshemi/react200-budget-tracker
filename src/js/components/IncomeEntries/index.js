983ed4e752d3d11ea155a2bea45780b86cf0c055
Yahshemi Walters <Yahshemi@gmail.com>
2019-02-11 11:23:39 -0800

    modified Summary/index.js

--- src/js/components/IncomeEntries/index.js
+++ src/js/components/IncomeEntries/index.js
@@ -1,3 +1,15 @@
+import { connect } from "react-redux";
 import IncomeEntries from "./IncomeEntries";
 
-export default IncomeEntries;
+function mapStoreToProps(store) {
+    return {
+       description: store.income.description,
+       amount: store.income.amount,
+       lineItems: store.income.lineItems 
+    };
+}
+
+// This might look odd but, connect returns a function,
+// that is then called with the component itself.
+
+export default connect (mapStoreToProps) (IncomeEntries);
